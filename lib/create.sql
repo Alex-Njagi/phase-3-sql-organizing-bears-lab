d1cb9c4e5e02ff4f57f2b998242e388a5c9e0f0c
Alex Njagi <1992anjagi@gmail.com>
2023-05-23 12:42:19 +0300

    Completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE Table bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INTEGER
+);
---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Mr. Chocolate", 14, "M", "Red", "Melancholic", 1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Rowdy", 4, "M", "Orange", "Sanguine", 1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Tabitha", 6, "F", "Yellow", "Choleric", 1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Sergeant Brown", 2, "F", "Green", "Melancholic", 1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Melissa", 20, "F", "Blue", "Phlegmatic", 0
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Grinch", 18, "M", "Indigo", "Sanguine", 1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    "Wendy", 5, "F", "Violet", "Choleric", 1
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+    NULL, 12, "M", "Black", "Sanguine", 0
+);
